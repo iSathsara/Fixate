458eb6d56dca1c337b425aac53bb9ce9318a24fd
iSathsara <isuru.dex@gmail.com>
2019-08-07 06:45:07 +0530

    game over

---
+++ app/src/main/java/com/example/uitesting/finall.java
@@ -0,0 +1,14 @@
+package com.example.uitesting;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class finall extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_finall);
+    }
+}
